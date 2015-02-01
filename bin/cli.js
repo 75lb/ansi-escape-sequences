dd3ff07656941d5b263f2a010d1618c896c264a5
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-02-01 11:59:53 +0000

    cli, default values

--- bin/cli.js
+++ bin/cli.js
@@ -3,14 +3,16 @@
 
 var ansi = require("../");
 
-var name = process.argv[2]; 
-var arg1 = process.argv[3];
+var name = process.argv[2];
+var name2 = process.argv[3];
+var arg1 = process.argv[4];
+var arg2 = process.argv[5];
 var seq = null;
-    
-if (typeof ansi[name] === "function"){
-    seq = ansi[name](arg1);
+
+if (typeof ansi[name][name2] === "function"){
+    seq = ansi[name][name2](arg1, arg2);
 } else {
-    seq = ansi[name];
+    seq = ansi[name][name2];
 }
 if (seq !== null) {
     process.stdout.write(seq);
