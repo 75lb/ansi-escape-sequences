c6ab34b167b79f6f7fcf60bb99efadf2c0524eba
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-02-01 11:40:27 +0000

    test

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ var test = require("tape");
 var ansi = require("../");
 
 test("format", function(t){
-    t.equal(ansi.format("clive", ["red", "underline"]), "\u001b[31;4mclive\u001b[0m");
+    t.equal(ansi.sgr.format("clive", ["red", "underline"]), "\u001b[31;4mclive\u001b[0m");
     t.end();
 });
