c842b62f460f9ad3b2f163bb4da4fdf843366feb
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-02-10 10:29:04 +0000

    added  option to ansi.format()

--- test/test.js
+++ test/test.js
@@ -10,3 +10,19 @@ test('inline format', function (t) {
   t.equal(ansi.format('before [red underline]{clive} after'), 'before \u001b[31;4mclive\u001b[0m after')
   t.end()
 })
+
+test('inline format, eachWord', function (t) {
+  t.equal(
+    ansi.format('one two', ['red', 'underline'], { eachWord: true }),
+    '\u001b[31;4mone\u001b[0m \u001b[31;4mtwo\u001b[0m'
+  )
+  t.end()
+})
+
+test('inline format, eachWord, newlines', function (t) {
+  t.equal(
+    ansi.format('one\ntwo', ['red', 'underline'], { eachWord: true }),
+    '\u001b[31;4mone\u001b[0m\n\u001b[31;4mtwo\u001b[0m'
+  )
+  t.end()
+})
