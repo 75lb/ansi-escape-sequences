ec624883afba7db9860448d48825b3c0866abb51
laino <lain@volafile.io>
2017-11-01 19:21:49 +0100

    Support nested inline styles and escaping them with '\'

--- test/test.js
+++ test/test.js
@@ -10,3 +10,12 @@ runner.test('format', function () {
 runner.test('inline format', function () {
   a.strictEqual(ansi.format('before [red underline]{clive} after'), 'before \u001b[31;4mclive\u001b[0m after')
 })
+
+runner.test('nested inline format', function () {
+  a.strictEqual(ansi.format('before [red underline]{inside [green]{nested}} after'),
+        'before \u001b[31;4minside \u001b[32mnested\u001b[0m\u001b[31;4m\u001b[0m after')
+})
+
+runner.test('escaped inline format', function () {
+  a.strictEqual(ansi.format('[red]{all this \\} is red} \\[green]{not green}'), '\u001b[31mall this \\} is red\u001b[0m \\[green]{not green}')
+})
