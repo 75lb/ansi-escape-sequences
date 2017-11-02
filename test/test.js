4b0b6ef666a7f6c0f603cfc1a563384ef97b345c
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-11-02 23:10:47 +0000

    formatting for readability

--- test/test.js
+++ test/test.js
@@ -4,39 +4,64 @@ const runner = new TestRunner()
 const a = require('assert')
 
 runner.test('format', function () {
-  a.strictEqual(ansi.format('clive', ['red', 'underline']), '\u001b[31;4mclive\u001b[0m')
+  a.strictEqual(
+    ansi.format('clive', ['red', 'underline']),
+    '\u001b[31;4mclive\u001b[0m'
+  )
 })
 
 runner.test('inline format', function () {
-  a.strictEqual(ansi.format('before [red underline]{clive} after'), 'before \u001b[31;4mclive\u001b[0m after')
+  a.strictEqual(
+    ansi.format('before [red underline]{clive} after'),
+    'before \u001b[31;4mclive\u001b[0m after'
+  )
 })
 
 runner.test('nested inline format', function () {
-  a.strictEqual(ansi.format('before [red underline]{inside [green]{nested}} after'),
-        'before \u001b[31;4minside \u001b[32mnested\u001b[0m\u001b[31;4m\u001b[0m after')
+  a.strictEqual(
+    ansi.format('before [red underline]{inside [green]{nested}} after'),
+    'before \u001b[31;4minside \u001b[32mnested\u001b[0m\u001b[31;4m\u001b[0m after'
+  )
 })
 
 runner.test('nested inline format 2', function () {
-  a.strictEqual(ansi.format('[red underline]{inside [green]{[bold]{nested} green}}'),
-        '\u001b[31;4minside \u001b[32m\u001b[1mnested\u001b[0m\u001b[31;4m\u001b[32m green\u001b[0m\u001b[31;4m\u001b[0m')
+  a.strictEqual(
+    ansi.format('[red underline]{inside [green]{[bold]{nested} green}}'),
+    '\u001b[31;4minside \u001b[32m\u001b[1mnested\u001b[0m\u001b[31;4m\u001b[32m green\u001b[0m\u001b[31;4m\u001b[0m'
+  )
 })
 
 runner.test('escaped inline format', function () {
-  a.strictEqual(ansi.format('[red]{all this \\} is red} \\[green]{not green}'), '\u001b[31mall this } is red\u001b[0m [green]{not green}')
+  a.strictEqual(
+    ansi.format('[red]{all this \\} is red} \\[green]{not green}'),
+    '\u001b[31mall this } is red\u001b[0m [green]{not green}'
+  )
 })
 
 runner.test('escaped escape sequence', function () {
-  a.strictEqual(ansi.format('\\[red]{not red} \\\\[red]{red\\\\}'), '[red]{not red} \\\u001b[31mred\\\u001b[0m')
+  a.strictEqual(
+    ansi.format('\\[red]{not red} \\\\[red]{red\\\\}'),
+    '[red]{not red} \\\u001b[31mred\\\u001b[0m'
+  )
 })
 
 runner.test('nested escape sequences', function () {
-  a.strictEqual(ansi.format('[red]{\\\\\\\\}'), '\u001b[31m\\\\\u001b[0m')
+  a.strictEqual(
+    ansi.format('[red]{\\\\\\\\}'),
+    '\u001b[31m\\\\\u001b[0m'
+  )
 })
 
 runner.test('unterminated format', function () {
-  a.strictEqual(ansi.format('[red]{uhoh'), 'uhoh')
+  a.strictEqual(
+    ansi.format('[red]{uhoh'),
+    'uhoh'
+  )
 })
 
 runner.test('dangling close', function () {
-  a.strictEqual(ansi.format('uhoh}'), 'uhoh}')
+  a.strictEqual(
+    ansi.format('uhoh}'),
+    'uhoh}'
+  )
 })
