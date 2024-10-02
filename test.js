1d01d60d6984aa1d52906e70ce6e631c44e5ca01
Lloyd Brookes <lloyd.brookes@gmail.com>
2024-10-02 16:04:40 +0100

    naturalise.. update Licence email

--- test.js
+++ test.js
@@ -1,32 +1,31 @@
-import TestRunner from 'test-runner'
 import ansi from 'ansi-escape-sequences'
 import { strict as a } from 'assert'
 
-const tom = new TestRunner.Tom()
+const [test, only, skip] = [new Map(), new Map(), new Map()]
 
-tom.test('format', function () {
+test.set('format', function () {
   const result = ansi.format('clive', ['red', 'underline'])
   a.equal(result, '\u001b[31m\u001b[4mclive\u001b[0m')
 })
 
-tom.test('format number', function () {
+test.set('format number', function () {
   const result = ansi.format(-100, ['red', 'underline'])
   a.equal(result, '\u001b[31m\u001b[4m-100\u001b[0m')
 })
 
-tom.test('format bg-rgb', function () {
+test.set('format bg-rgb', function () {
   const result = ansi.format('hello', 'bg-rgb(100,0,100)')
   a.equal(result, '\u001b[48;2;100;0;100mhello\u001b[0m')
 })
 
-tom.test('inline format', function () {
+test.set('inline format', function () {
   const result = ansi.format('before [red underline]{clive} after')
   a.equal(result, 'before \u001b[31m\u001b[4mclive\u001b[0m after')
 })
 
-tom.test('inline format rgb', function () {
+test.set('inline format rgb', function () {
   const result = ansi.format('before [rgb(150,0,150) underline]{clive} after')
   a.equal(result, 'before \u001b[38;2;150;0;150m\u001b[4mclive\u001b[0m after')
 })
 
-export default tom
+export { test, only, skip }
